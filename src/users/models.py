44a54998c75920fbe26fb23a47df2f53f8f41588
Orlan Quijada <orlantquijada@gmail.com>
2022-02-07 00:55:10 +0800

    feat: (ORL-18) User model

--- src/users/models.py
+++ src/users/models.py
@@ -1,3 +1,11 @@
-from django.db import models
+from django.contrib.auth.models import AbstractUser, PermissionsMixin
 
-# Create your models here.
+
+class User(
+    AbstractUser,
+    PermissionsMixin,
+):
+    REQUIRED_FIELDS = []
+
+    def __str__(self) -> str:
+        return f"{self.id} / {self.get_full_name()}"
