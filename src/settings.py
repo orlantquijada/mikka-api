fdb9a4aeef9ecebd78190f5e7b019b3058b26f44
Orlan Quijada <orlantquijada@gmail.com>
2022-02-12 23:39:12 +0800

    feat: heroku deployment config

--- src/settings.py
+++ src/settings.py
@@ -9,8 +9,9 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
 from pathlib import Path
+
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,8 +77,12 @@ WSGI_APPLICATION = "src.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "mikka_db",
+        "USER": "mikka",
+        "PASSWORD": "mikka",
+        "HOST": "",
+        "PORT": 5432,
     }
 }
 
@@ -124,3 +129,5 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "users.User"
+
+django_heroku.settings(locals())
